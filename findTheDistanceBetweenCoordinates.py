861f8d1cd49f79fc9e8569e095302da36e59892e
Chanwoo Kim <84301139+aries043@users.noreply.github.com>
2022-06-04 21:39:23 +0900

    Create Latitude and hardness tests

--- findTheDistanceBetweenCoordinates.py
+++ findTheDistanceBetweenCoordinates.py
@@ -7,4 +7,11 @@ class GeoUtil:
     def degree2radius(degree):
         return degree * (math.pi / 180)
 
-    
+    @staticmethod
+    def get_harversion_distance(x1, y1, x2, y2, round_decimal_digits=5):
+        if x1 is None or y1 is None or x2 is None or y2 is None:
+            return None
+        assert isinstance(x1, numbers.Number) and -180 <= x1 and x1 <= 180
+        assert isinstance(y1, numbers.Number) and -90 <= y1 and y1 <= 90
+        assert isinstance(x2, numbers.Number) and -180 <= x2 and x2 <= 180
+        assert isinstance(y2, numbers.Number) and -90 <= y2 and y2 <= 90
