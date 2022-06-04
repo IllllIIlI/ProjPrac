9a544eac249671cb3aa328dca7c8f0e63dd8ef91
IllllIIlI <g.w.park401@gmail.com>
2022-06-04 22:38:16 +0900

    preprocessing2, import modules

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import chargerGuide
 import chargingMethod
+import findCurLoc
 import pandas as pd
 
 cur_lat = 0.0
@@ -15,3 +16,7 @@ print("Please enter the area where you want to find the location of the charging
 str_want_go = input()
 int_line = excel_source['주소'].str.contains(str_want_go)
 want_go_excel = excel_source[int_line]
+want_go_excel.to_excel(
+    'ProjPrac/result.xlsx', sheet_name='Result')
+
+cur_lat, cur_lng = findCurLoc.Find()
