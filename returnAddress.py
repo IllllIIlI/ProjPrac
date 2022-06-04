058f00ee488ceb009a201493c779d848cb260a23
CBNU-kimjimin <92361394+CBNU-kimjimin@users.noreply.github.com>
2022-06-04 23:21:33 +0900

    위도 경도 리턴

--- returnAddress.py
+++ returnAddress.py
@@ -3,3 +3,6 @@ import googlemaps
 def getloc(addr):
     gmaps = googlemaps.Client(key='구글키를 입력해주세요')
     geocode_result = gmaps.geocode(addr)
+    n_lat = geocode_result[0]['geometry']['location']['lat']
+    n_lng = geocode_result[0]['geometry']['location']['lng']
+    return n_lat, n_lng
