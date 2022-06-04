5c74a87be09c7d0ea7eaa93ae23c0283e7755958
Chanwoo Kim <84301139+aries043@users.noreply.github.com>
2022-06-04 20:04:08 +0900

    Import findCurLoc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import chargingMethod
 import pandas as pd
+import findCurLoc
 
 chargingMethod.guide()
 excel_source = pd.read_excel('C:/Users/cksdn/PycharmProjects/OSS_Project_04/전기차-충전소-설치현황_20220316.xlsx', usecols=[1, 2, 3, 4, 5])
@@ -8,3 +9,5 @@ str_want_go = input()
 int_line = excel_source['주소'].str.contains(str_want_go)
 want_go_excel = excel_source[int_line]
 want_go_excel.to_excel('C:/Users/cksdn/PycharmProjects/OSS_Project_04/result.xlsx', sheet_name= 'Result')
+
+cur_lat, cur_lng = findCurLoc.find()
