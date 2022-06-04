1b94184effd10f0582384b5f4fe81aac82f6538a
CBNU-kimjimin <92361394+CBNU-kimjimin@users.noreply.github.com>
2022-06-04 23:25:48 +0900

    4개 변수

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ import findTheDistanceBetweenCoordinates as Dis
 import chargingStationMarker
 import fastChargerMarker
 import slowChargerMarker
+
+cur_lat = 0.0
+cur_lng = 0.0
+dst_lat = 0.0
+dst_lng = 0.0
