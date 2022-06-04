c42dd163075d2429d792d7078dead2c810662a3f
Chanwoo Kim <84301139+aries043@users.noreply.github.com>
2022-06-04 20:06:32 +0900

    Excel read, print instructions

--- main.py
+++ main.py
@@ -11,3 +11,7 @@ want_go_excel = excel_source[int_line]
 want_go_excel.to_excel('C:/Users/cksdn/PycharmProjects/OSS_Project_04/result.xlsx', sheet_name= 'Result')
 
 cur_lat, cur_lng = findCurLoc.find()
+
+excel_source = pd.read_excel('C:/Users/cksdn/PycharmProjects/OSS_Project_04/result.xlsx', usecols=[1, 2, 3, 4, 5])
+print("Please enter the type of car you want.\n"
+      "SM3 Z.E, 레이EV, 소울EV, 닛산리프, 아이오닉EV, BMW i3, 스파크EV, 볼트EV
