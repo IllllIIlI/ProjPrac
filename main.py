d0f12511dff004463a1da1b57671fd30f490d49e
IllllIIlI <g.w.park401@gmail.com>
2022-06-04 22:39:16 +0900

    preprocessing3, print cars

--- main.py
+++ main.py
@@ -20,3 +20,7 @@ want_go_excel.to_excel(
     'ProjPrac/result.xlsx', sheet_name='Result')
 
 cur_lat, cur_lng = findCurLoc.Find()
+excel_source = pd.read_excel(
+    'ProjPrac/result.xlsx', usecols=[1, 2, 3, 4, 5])
+print("Please enter the type of car you want.\n"
+      "SM3 Z.E, 레이EV, 소울EV, 닛산리프, 아이오닉EV, BMW i3, 스파크EV, 볼트EV, 테슬라")
