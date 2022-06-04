a2ccb3c8486e170ec3a1c35e606918167b8c8b47
CBNU-kimjimin <92361394+CBNU-kimjimin@users.noreply.github.com>
2022-06-04 23:28:17 +0900

    Excel extraction

--- main.py
+++ main.py
@@ -18,3 +18,7 @@ chargingMethod.guide()
 
 excel_source = pd.read_excel('전기차-충전소-설치현황_20220316.xlsx', usecols=[1, 2, 3, 4, 5])
 print("충전소 위치를 찾고 싶은 도시를 입력하세요.")
+str_want_go = input()
+int_line = excel_source['주소'].str.contains(str_want_go)
+want_go_excel = excel_source[int_line]
+want_go_excel.to_excel('result.xlsx', sheet_name= 'Result')
