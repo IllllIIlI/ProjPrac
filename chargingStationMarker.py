14e9063b114594205d2d6786a7b584cfdddb47b2
CBNU-kimjimin <92361394+CBNU-kimjimin@users.noreply.github.com>
2022-06-04 23:09:10 +0900

    create 2 list using for instriments

--- chargingStationMarker.py
+++ chargingStationMarker.py
@@ -7,3 +7,10 @@ def chargingStationMarker(g_map):
     excel_source = pd.read_excel('result2.xlsx', usecols=[2])
     lat = []
     lng = []
+    
+    k = excel_source.shape[0]
+    
+    for cnt in range(k):
+        chg_lat, chg_lng = returnAddress.getloc(str(excel_source.loc[cnt]))
+        lat.append(chg_lat)
+        lng.append(chg_lng)
