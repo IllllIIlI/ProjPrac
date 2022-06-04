1e912f6b9f642c10c68d36c14e76266034fea54e
IllllIIlI <g.w.park401@gmail.com>
2022-06-04 22:37:16 +0900

    preprocessing

--- main.py
+++ main.py
@@ -11,4 +11,7 @@ chargingMethod.guide()
 
 excel_source = pd.read_excel(
     'ProjPrac/전기차-충전소-설치현황_20220316.xlsx', usecols=[1, 2, 3, 4, 5])
-print()
+print("Please enter the area where you want to find the location of the charging station. ex)청주")
+str_want_go = input()
+int_line = excel_source['주소'].str.contains(str_want_go)
+want_go_excel = excel_source[int_line]
