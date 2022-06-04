498812d73ec450da7d284120252d659826b0355d
Chanwoo Kim <84301139+aries043@users.noreply.github.com>
2022-06-04 21:03:49 +0900

    Excel Create 7, 8

--- main.py
+++ main.py
@@ -78,3 +78,7 @@ if charger == "Fast":
     want_go_excel.to_excel('C:/Users/cksdn/PycharmProjects/OSS_Project_04/result2_5.xlsx', sheet_name='Result')
     want_go_excel = excel_source[int_line6]
     want_go_excel.to_excel('C:/Users/cksdn/PycharmProjects/OSS_Project_04/result2_6.xlsx', sheet_name='Result')
+    want_go_excel = excel_source[int_line7]
+    want_go_excel.to_excel('C:/Users/cksdn/PycharmProjects/OSS_Project_04/result2_7.xlsx', sheet_name='Result')
+    want_go_excel = excel_source[int_line8]
+    want_go_excel.to_excel('C:/Users/cksdn/PycharmProjects/OSS_Project_04/result2_8.xlsx', sheet_name='Result')
