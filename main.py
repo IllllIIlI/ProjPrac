6decba98cc3db1a28903aed3b298531fa129a94a
Chanwoo Kim <84301139+aries043@users.noreply.github.com>
2022-06-04 18:37:20 +0900

    Import pandas, Read Excel

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import chargingMethod
+import pandas as pd
 
 chargingMethod.guide()
+excel_source = pd.read_excel('C:/Users/cksdn/PycharmProjects/OSS_Project_04/전기차-충전소-설치현황_20220316.xlsx', usecols=[1, 2, 3, 4, 5])
+print("Please enter the area where you want to find the location of the charging station. ex)청주")
