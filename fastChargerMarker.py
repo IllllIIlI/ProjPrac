307488748b6c6417ed3b97c0af3df2a3ff383d9a
CBNU-kimjimin <92361394+CBNU-kimjimin@users.noreply.github.com>
2022-06-04 23:11:25 +0900

    Make def, 2 list

--- fastChargerMarker.py
+++ fastChargerMarker.py
@@ -2,3 +2,8 @@ import pandas as pd
 import returnAddress
 import folium as g
 from folium.plugins import MarkerCluster
+
+def fastChargerMarker(g_map):
+    excel_source = pd.read_excel('result2.xlsx', usecols=[2])
+    lat = []
+    lng = []
