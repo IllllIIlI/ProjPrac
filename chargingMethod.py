905f6cb9b584e0d5a1af13491b0c845b9f7ffe62
IllllIIlI <g.w.park401@gmail.com>
2022-06-04 22:19:59 +0900

    Add print contents

--- chargingMethod.py
+++ chargingMethod.py
@@ -1,5 +1,8 @@
 def guide():
-    print()
+    print("If you want to be informed about how to charge the electric vehicle, please enter Y.")
     answer = input()
     if answer == "Y":
-        print()
+        print("1.Stop in front of the charger\n" "2.Check the charging connector for my car\n"
+              "3.Connect to the vehicle\n" "4.Set charge amount\n"
+              "5.Pay the charging fee with the previously issued charging card or credit card\n"
+              "6.Clean up connectors and instruments after charging is completed")
