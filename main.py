f345f546feca4bef9addc1049c20d83ac0c22053
IllllIIlI <g.w.park401@gmail.com>
2022-06-04 22:36:14 +0900

    import pandas, read exel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import chargerGuide
 import chargingMethod
+import pandas as pd
 
 cur_lat = 0.0
 cur_lng = 0.0
@@ -7,3 +8,7 @@ dst_lat = 0.0
 dst_lng = 0.0
 
 chargingMethod.guide()
+
+excel_source = pd.read_excel(
+    'ProjPrac/전기차-충전소-설치현황_20220316.xlsx', usecols=[1, 2, 3, 4, 5])
+print()
