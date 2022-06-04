21ac3066e906bba816bef3b419a7964b4984e4bd
Chanwoo Kim <84301139+aries043@users.noreply.github.com>
2022-06-04 21:07:49 +0900

    Create Excel 3, 4

--- main.py
+++ main.py
@@ -96,3 +96,5 @@ if charger == "Fast":
 elif charger == "Slow":
     int_line1 = excel_source['완속충전기(대)'].astype(str).str.contains("1")
     int_line2 = excel_source['완속충전기(대)'].astype(str).str.contains("2")
+    int_line3 = excel_source['완속충전기(대)'].astype(str).str.contains("3")
+    int_line4 = excel_source['완속충전기(대)'].astype(str).str.contains("4")
