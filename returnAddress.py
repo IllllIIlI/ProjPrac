45c0edb6078bb6aa91f7bdef4b64a79cb991f658
IllllIIlI <g.w.park401@gmail.com>
2022-06-04 23:14:46 +0900

    return lat, lng

--- returnAddress.py
+++ returnAddress.py
@@ -4,3 +4,6 @@ import googlemaps
 def GetLoc(addr):
     gmaps = googlemaps.Client(key='')
     geocode_result = gmaps.geocode(addr)
+    lat = geocode_result[0]['geometry']['location']['lat']
+    lng = geocode_result[0]['geometry']['location']['lng']
+    return lat, lng
