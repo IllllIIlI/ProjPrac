2531455af7bbecb0fb42f8c46db211e4979ff69c
IllllIIlI <g.w.park401@gmail.com>
2022-06-04 23:07:57 +0900

    def func

--- charginStationMarker.py
+++ charginStationMarker.py
@@ -2,3 +2,9 @@ import pandas as pd
 import returnAddress
 import folium as g
 from folium.plugins import MarkerCluster
+
+
+def chargingStationMarker(map):
+    excel_source = pd.read_excel('ProjPrac/result_charger.xlsx', usecols=[2])
+    lat = []
+    lng = []
