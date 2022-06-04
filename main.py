138ea5b0daaee5289f291b2a662a8e8da9862593
IllllIIlI <g.w.park401@gmail.com>
2022-06-04 22:48:36 +0900

    output xls8

--- main.py
+++ main.py
@@ -79,3 +79,9 @@ if charger == "Fast":
                    'ProjPrac/result_charger_5.xlsx', 'ProjPrac/result_charger_6.xlsx', 'ProjPrac/result_charger_7.xlsx', 'ProjPrac/result_charger_8.xlsx',
                    'ProjPrac/result_charger_9.xlsx']
     excels = [pd.ExcelFile(name) for name in excel_names]
+    frames = [x.parse(x.sheet_names[0], header=None, index_col=None)
+              for x in excels]
+    frames[1:] = [df[1:] for df in frames[1:]]
+    combined = pd.concat(frames)
+    combined.to_excel(
+        "ProjPrac/result_charger.xlsx", header=False, index=False)
