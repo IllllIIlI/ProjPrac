ae7af82df00593497df909ac196f4dbc311dfc38
CBNU-kimjimin <92361394+CBNU-kimjimin@users.noreply.github.com>
2022-06-04 23:26:55 +0900

    Use function. read excel

--- main.py
+++ main.py
@@ -13,3 +13,8 @@ cur_lat = 0.0
 cur_lng = 0.0
 dst_lat = 0.0
 dst_lng = 0.0
+
+chargingMethod.guide()
+
+excel_source = pd.read_excel('전기차-충전소-설치현황_20220316.xlsx', usecols=[1, 2, 3, 4, 5])
+print("충전소 위치를 찾고 싶은 도시를 입력하세요.")
