89b9e6ae6851ef29cdd11aeb4d09114344af5dbf
IllllIIlI <g.w.park401@gmail.com>
2022-06-04 22:50:53 +0900

    output xls12

--- main.py
+++ main.py
@@ -95,3 +95,7 @@ elif charger == "Slow":
     int_line6 = excel_source['완속충전기(대)'].astype(str).str.contains("6")
     int_line7 = excel_source['완속충전기(대)'].astype(str).str.contains("7")
     int_line8 = excel_source['완속충전기(대)'].astype(str).str.contains("8")
+    int_line9 = excel_source['완속충전기(대)'].astype(str).str.contains("9")
+    want_go_excel = excel_source[int_line1]
+    want_go_excel.to_excel(
+        'ProjPrac/result_charger_1.xlsx', sheet_name='Normal_Charger')
